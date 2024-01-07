b970a1acec74259cd5edfef803aeb24578d3606c
Samet Demiralay <demiralayahmetsamet@gmail.com>
2024-01-07 18:30:08 +0300

    updated second part

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
-// mobile nav
-const mobileNav = document.querySelector('.mnav')
-const closeBtn = document.querySelector('.mnav__close-btn')
-const closeBtnIcon = document.querySelector('.mnav__close-btn-icon')
+/*
+ *** mobile nav
+ */
+const mobileNav = document.querySelector('.mnav');
+const closeBtn = document.querySelector('.mnav__close-btn');
+const closeBtnIcon = document.querySelector('.mnav__close-btn-icon');
 
-const navOpenedClass = 'left-0'
-const navClosedClass = '-left-[300px]'
-const arrowLeftClass = 'ri-arrow-left-s-line'
-const arrowRightClass = 'ri-arrow-right-s-line'
+const navOpenedClass = 'left-0';
+const navClosedClass = '-left-[300px]';
+const arrowLeftClass = 'ri-arrow-left-s-line';
+const arrowRightClass = 'ri-arrow-right-s-line';
 
-closeBtn.addEventListener("click", () => {
-    if(mobileNav.classList.contains(navClosedClass)){
-        mobileNav.classList.toggle(navOpenedClass)
+closeBtn.addEventListener('click', () => {
+  if (mobileNav.classList.contains(navClosedClass)) {
+    mobileNav.classList.toggle(navOpenedClass);
 
-        closeBtnIcon.classList.toggle(arrowLeftClass)
-        closeBtnIcon.classList.toggle(arrowRightClass)
-    }
-})
+    closeBtnIcon.classList.toggle(arrowLeftClass);
+    closeBtnIcon.classList.toggle(arrowRightClass);
+  }
+});
+
+/*
+ *** slider
+ */
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+/*
+ *** faq
+ */
+const faqItems = document.querySelectorAll('.faq__item');
+
+faqItems.forEach((item) => {
+  const faqBtn = item.querySelector('.faq__btn');
+
+  item.addEventListener('click', () => {
+    const isOpen = item.classList.toggle('open');
+    const iconClass = isOpen ? 'ri-subtract-fill' : 'ri-add-fill'
+    const iconElement = faqBtn.querySelector('i');
+    iconElement.classList = `${iconClass} text-2xl`
+  });
+});
