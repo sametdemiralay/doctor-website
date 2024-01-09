545e9243906ff5be663e5acdeb48bc8e8fd13a81
Samet Demiralay <demiralayahmetsamet@gmail.com>
2024-01-09 21:32:47 +0300

    updated final

--- js/main.js
+++ js/main.js
@@ -41,8 +41,95 @@ faqItems.forEach((item) => {
 
   item.addEventListener('click', () => {
     const isOpen = item.classList.toggle('open');
-    const iconClass = isOpen ? 'ri-subtract-fill' : 'ri-add-fill'
+    const iconClass = isOpen ? 'ri-subtract-fill' : 'ri-add-fill';
     const iconElement = faqBtn.querySelector('i');
-    iconElement.classList = `${iconClass} text-2xl`
+    iconElement.classList = `${iconClass} text-2xl`;
   });
+});
+
+/*
+ *** scroll reveal animations
+ */
+const sr = ScrollReveal({
+  origin: 'bottom',
+  distance: '60px',
+  duration: 2000,
+  delay: 600,
+  // reset: true // reset animation
+});
+
+// hero
+sr.reveal('.hero__text', { origin: 'top' });
+sr.reveal('.hero__img');
+
+// stats
+sr.reveal('.stats__item', {
+  delay: 600,
+  distance: '100px',
+  interval: 100,
+  origin: 'top'
+});
+
+// services
+sr.reveal('.services');
+sr.reveal('.services__top');
+sr.reveal('.services__item', {
+  delay: 600,
+  distance: '100px',
+  interval: 100,
+  origin: 'bottom'
+});
+
+// appointment
+sr.reveal('.appointment__title');
+sr.reveal('.appointment__form');
+
+// testimonials
+sr.reveal('.testimonial');
+sr.reveal('.testimonial__container');
+
+// team
+sr.reveal('.team__title');
+sr.reveal('.team__slider');
+
+// faq
+sr.reveal('.faq__title');
+sr.reveal('.faq__item', {
+  delay: 600,
+  distance: '100px',
+  interval: 100,
+  origin: 'bottom'
+});
+
+// departments
+sr.reveal('.departments__bg');
+sr.reveal('.departments__container');
+
+// blog
+sr.reveal('.blog__title');
+sr.reveal('.blog__post', {
+  delay: 600,
+  distance: '100px',
+  interval: 100,
+  origin: 'bottom'
+});
+
+// brands
+sr.reveal('.brands__logo', {
+  delay: 600,
+  distance: '100px',
+  interval: 100,
+  origin: 'bottom'
+});
+
+// newsletter
+sr.reveal('.newsletter');
+sr.reveal('.newsletter__container');
+
+// footer
+sr.reveal('.footer__item', {
+  delay: 600,
+  distance: '100px',
+  interval: 100,
+  origin: 'bottom'
 });
